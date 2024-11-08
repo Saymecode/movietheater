d6b592059ef8b833cc3914889645d56fc956f837
Fedor Philippov <saymecode@saymecode.local>
2024-11-07 16:03:34 -0800

    packages/first setup/database

--- config/database.php
+++ config/database.php
@@ -34,14 +34,13 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE') ? database_path(env('DB_DATABASE')) : database_path('database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
             'journal_mode' => null,
             'synchronous' => null,
         ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
